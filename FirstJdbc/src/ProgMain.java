d7a4ca30346ad6214e4156e9dab8a5a9aa18fe7a
kunwaraman <amankunwar8472@gmail.com>
2024-11-13 17:53:02 +0530

    firstcommit

---
+++ FirstJdbc/src/ProgMain.java
@@ -0,0 +1,36 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+public class ProgMain {
+    public static void main(String[] args) {
+        try{
+            String drivername="com.mysql.cj.jdbc.Driver";
+            Class.forName(drivername);
+            String url="jdbc:mysql://localhost:3306/world";
+            String username="root";
+            String pswd = "aman123";
+            Connection conn = DriverManager.getConnection(url,username,pswd);
+
+            Statement st = conn.createStatement();
+            String sql = "select * from city";
+            ResultSet rs = st.executeQuery(sql);
+
+            while (rs.next()){
+                int id=rs.getInt(1);
+                String Cname=rs.getString(2);
+                String Ccode = rs.getString(3);
+                String distct= rs.getString(4);
+                int popu = rs.getInt(5);
+
+                System.out.println(id+" " +Cname+" " +Ccode+" "+distct+" " +popu);
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+
+    }
+}
